3d8011540985f0fb94399318d5d1ba8ec5e52438
Gin <57712394+ginderick@users.noreply.github.com>
2022-06-08 22:11:18 +0800

    Update CRUD functions

--- app/api/api_v1/endpoints/posts.py
+++ app/api/api_v1/endpoints/posts.py
@@ -1,3 +1,4 @@
+from msilib import schema
 from fastapi import APIRouter, Response
 from starlette import status
 from typing import List, Any
@@ -18,6 +19,7 @@ post_2.description = "hi"
 
 posts = [post_1, post_2]
 
+
 @router.get("/", response_model=List[schemas.Post])
 def read_posts() -> Any:
     """
@@ -25,20 +27,29 @@ def read_posts() -> Any:
     """
     return posts
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_posts(id: str, value: str):
-    posts[id] = value
-    return posts[id]
+@router.post("/", response_model=schemas.Post, status_code=status.HTTP_201_CREATED)
+async def create_posts(post_in: schemas.PostCreate):
+    """
+    Create new post.
+    """
+    new_post = Post(title=post_in.title, description=post_in.description)
+    posts.append(new_post)
+    return new_post
 
-@router.put("/{id}", status_code=status.HTTP_200_OK)
-async def update_posts(id: str, value: str):
-    posts[id] = value
-    return posts[id]
 
-@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_posts(id: str):
+@router.put("/{title}", status_code=status.HTTP_200_OK)
+async def update_posts(title: str, description: str):
+    for post in posts:
+        if post.title == title:
+            post.description = description
+            return post
+
+@router.delete("/{title}", response_model=schemas.Post)
+async def delete_posts(title: str):
+
+    for post in posts:
+        if post.title == title:
+            posts.remove(post)
+            return post
     
-    if id in posts:
-        del posts[id]
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
     return Response(status_code=status.HTTP_400_BAD_REQUEST)
